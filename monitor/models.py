2d41ded2383f9be373fcce185c95b3e86df09313
JJ-Choi1999 <2486595624@qq.com>
2025-04-09 21:26:59 +0800

    models 指定表名

--- monitor/models.py
+++ monitor/models.py
@@ -2,17 +2,26 @@ from django.db import models
 
 class City(models.Model):
 
+    class Meta:
+        db_table = "city"
+
     province = models.CharField(max_length=255, help_text="一级行政区名")
     city = models.CharField(max_length=255, help_text="二级行政区名")
     location = models.CharField(max_length=255, help_text="具体地址")
 
 class IDC(models.Model):
 
+    class Meta:
+        db_table = "idc"
+
     city_id = models.BigIntegerField(help_text="关联城市id")
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
 
 class Host(models.Model):
+
+    class Meta:
+        db_table = "host"
 
     STATUS_CHOICES = [
         ('active', 'Active'),
@@ -28,6 +37,9 @@ class Host(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class PasswordHistory(models.Model):
+
+    class Meta:
+        db_table = "password_history"
 
     host_id = models.BigIntegerField(help_text="主机关联id")
     old_password = models.CharField(max_length=255)
--- monitor/views/city_view.py
+++ monitor/views/city_view.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
+import json
+
+from django.db import transaction
+
+from monitor.models import City
 from utils.req_handler import req_handler
 
 @req_handler
 def create_city(request):
-    pass
+
+    if request.method != 'POST':
+        raise Exception('请求方式有误')
+
+    datas = json.loads(request.body.decode('utf-8'))
+    city_objs = []
+
+    with transaction.atomic():
+        for data in datas:
+            city_obj = City(
+                province=data.get('province'),
+                city=data.get('city'),
+                location=data.get('location'),
+            )
+            city_objs.append(city_obj)
+
+        City.objects.bulk_create(city_objs)
 
 @req_handler
 def update_city(request):
