c192c0b3234f0d629a21384fc1cc82130b8e79d2
JJ-Choi1999 <2486595624@qq.com>
2025-04-10 11:44:50 +0800

    调整加密

--- utils/aes_util.py
+++ utils/aes_util.py
@@ -2,7 +2,9 @@ from cryptography.fernet import Fernet
 
 # [todo] 需要通过配置读取
 # 生成密钥（需安全保存）
-key = Fernet.generate_key()
+key_str = 'CkM6G_DpTVN0iU5C6fKDNJGojWhWmdbYn5Gp7ilAZNc='
+key = key_str.encode('utf-8')
+print(f'key: {key}')
 cipher = Fernet(key)
 
 def encrypt_text(text: str) -> bytes:
